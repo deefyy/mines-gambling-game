8d804630a0482f6c9e824722e4ca00b1f660c9e3
TimeToMakeDonuts <bartekobox@gmail.com>
2025-02-10 20:05:40 +0100

    Fixed Fixed login css

--- app/login/page.js
+++ app/login/page.js
@@ -1,15 +1,41 @@
-import { login, signup } from './actions'
+'use client';
+
+import { login, signup } from './actions';
 import styles from './page.module.css';
 
 export default function LoginPage() {
   return (
-    <form className={styles.form}>
-      <label htmlFor="email" className={styles.label}>Email:</label>
-      <input id="email" name="email" type="email" required className={styles.inputField} />
-      <label htmlFor="password" className={styles.label}>Password:</label>
-      <input id="password" name="password" type="password" required className={styles.inputField} />
-      <button formAction={login} className={styles.btn}>Log in</button>
-      <button formAction={signup} className={styles.btn}>Sign up</button>
-    </form>
+    <div className={styles.container}>
+      <div className={styles.formWrapper}>
+        <form className={styles.form}>
+          <label htmlFor="email" className={styles.label}>
+            Email:
+          </label>
+          <input
+            id="email"
+            name="email"
+            type="email"
+            required
+            className={styles.inputField}
+          />
+          <label htmlFor="password" className={styles.label}>
+            Password:
+          </label>
+          <input
+            id="password"
+            name="password"
+            type="password"
+            required
+            className={styles.inputField}
+          />
+          <button formAction={login} className={styles.btn}>
+            Log in
+          </button>
+          <button formAction={signup} className={styles.btn}>
+            Sign up
+          </button>
+        </form>
+      </div>
+    </div>
   );
 }
